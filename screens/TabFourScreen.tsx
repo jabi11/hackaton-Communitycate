3f08eaada818de9b23e36986502ab4a140e8e57d
Jan <jas.biernacki@gmail.com>
2021-12-12 06:30:21 +0100

    screen 4

--- screens/TabFourScreen.tsx
+++ screens/TabFourScreen.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import { ScrollView, StyleSheet, Image } from 'react-native';
+import { CardNoHero } from '../components/CardNoHero';
 import { Text, View } from '../components/Themed';
 
 export default function TabFourScreen() {
+  const avatar1 = require('../assets/images/avatar6.png');
+  const avatar2 = require('../assets/images/avatar10.png');
+  const avatar3 = require('../assets/images/avatar8.png')
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Four</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabFourScreen.tsx" />
+      <Image style={styles.logoImage} resizeMode={'contain'} source={require('../assets/images/communitycate.png')} />
+      <ScrollView>
+        <CardNoHero username='sąsiadka Kasia' avatar={avatar1} post='Cześć,
+Wymienię się miejscami parkingowymi na stałe, żeby miec bliżej do swojego mieszkania.'/>
+        <CardNoHero username='sąsiad Krzysztof' avatar={avatar2} post='Hej,
+Rzadko używam swojego termomixa i chętnie pożyczę, jakby ktoś potrzebował. '/>
+        <CardNoHero username='sąsiadka Krystyna' avatar={avatar3} post='Witajcie sąsiedzi,
+        Chciałabym się z kimś wymienić kiszonkami.'/>
+      </ScrollView>
     </View>
   );
 }
@@ -28,5 +37,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  logoImage: {
+    width: 340,
+    marginTop: 30
   },
 });
