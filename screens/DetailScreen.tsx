d952e0344f61891e8a5e8ca8901181c37117ae5d
KryNak <krystian.nakielski200397@gmail.com>
2021-12-12 09:13:30 +0100

    Added info view

--- screens/DetailScreen.tsx
+++ screens/DetailScreen.tsx
@@ -1,17 +1,34 @@
 import * as React from 'react';
 import { StyleSheet, TouchableOpacity, Image, TextInput, ImageBackground, Button } from 'react-native';
 import { Text, View } from '../components/Themed';
-import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+
+const avatar1 = require('../assets/images/avatar1.png');
+const hero2 = require('../assets/images/hero2.png');
 
 import { RootStackScreenProps } from '../types';
-import { Header } from 'react-native/Libraries/NewAppScreen';
 
 export default function DetailScreen({ navigation }: RootStackScreenProps<'Detail'>) {
   return (
     <View style={styles.container}>
-        <Button title='back' onPress={() => navigation.replace('Root')}></Button>
-        <Button title='Contact' onPress={() => navigation.replace('Contact')}></Button>
-        <Text>DETAIL</Text>
+      <View style={styles.upperContainerPart}>
+        <Image source={avatar1} style={styles.avatar}/>
+        <View>
+            <Text style={styles.text1}>Zofia</Text>
+            <Text>600 metrów stąd</Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+          <Text style={styles.typeText}>Pożyczę</Text>
+          <FontAwesome name="user" style={styles.userIcon}/>
+          <Text style={styles.username}>sąsiadka Zofia</Text>
+          <Text style={styles.postText}>
+            Hej, Chętnie pożyczę na weekend parownicę do ubrań w zamian za szarlotkę. 
+          </Text>
+          <Image source={hero2} style={styles.postPhoto}/>
+      </View>
+      <Button title='back' onPress={() => navigation.replace('Root')}></Button>
+      <Button title='Contact' onPress={() => navigation.replace('Contact')}></Button>
     </View>
   );
 }
@@ -22,4 +39,61 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
+  card: {
+    marginTop: 20,
+    backgroundColor: 'white',
+    borderRadius: 29,
+    borderColor: '#F49819',
+    borderWidth: 1,
+    height: 306,
+  },
+  typeText: {
+    color: '#F49819',
+    marginTop: 14,
+    marginLeft: 17,
+    textDecorationLine: 'underline',
+  },
+  userIcon: {
+    marginTop: 16,
+    marginLeft: 17
+  },
+  username: {
+    position: 'absolute',
+    top: 43,
+    left: 37,
+    fontSize: 14,
+    color: 'black'
+  },
+  avatar: {
+    alignSelf: 'flex-start',
+    borderRadius: 65,
+    backgroundColor: 'black'
+  },
+  postText: {
+    fontSize: 14,
+    maxWidth: 280,
+    marginTop: 20,
+    marginLeft: 17,
+    color: 'black'
+  },
+  postPhoto: {
+    backgroundColor: 'white',
+    top: 20,
+    width: 314,
+    height: 171,
+    borderTopLeftRadius: 22,
+    borderTopRightRadius: 22,
+    borderBottomRightRadius: 29,
+    borderBottomLeftRadius: 29
+  },
+  upperContainerPart: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: 150
+  },
+  text1: {
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 24
+  }
 });
