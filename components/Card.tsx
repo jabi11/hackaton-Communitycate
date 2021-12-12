852560a69d04fb03ebb744e9635f24a7feb7e5f4
Jan <jas.biernacki@gmail.com>
2021-12-12 04:14:27 +0100

    card

---
+++ components/Card.tsx
@@ -0,0 +1,68 @@
+import { View, StyleSheet, Text } from 'react-native'
+import React from 'react';
+import { FontAwesome } from '@expo/vector-icons';
+
+export const Card = (props: any) => 
+        <View style={styles.card}>
+            <Text style={styles.typeText}>Pozycze</Text>
+            <FontAwesome name="user" style={styles.userIcon}/>
+            <Text style={styles.username}>sąsiadka Zofia</Text>
+            <View style={styles.avatar}/>
+            <Text style={styles.postText}>Hej, Chętnie pożyczę na weekend parownicę do ubrań w zamian za szarlotkę.</Text>
+            <View style={styles.postPhoto}/>
+        </View>
+
+const styles = StyleSheet.create({
+    card: {
+        width: 314,
+        height: 314,
+        backgroundColor: 'white',
+        borderRadius: 29,
+        borderColor: '#F49819',
+        borderWidth: 1
+    },
+    typeText: {
+        color: '#F49819',
+        marginTop: 14,
+        marginLeft: 17
+    },
+    userIcon: {
+        marginTop: 16,
+        marginLeft: 17
+    },
+    username: {
+        position: 'absolute',
+        top: 43,
+        left: 37,
+        fontSize: 14,
+        color: 'black'
+    },
+    avatar: {
+        position: 'absolute',
+        top: 18,
+        right: 12,
+        width: 62,
+        height: 62,
+        borderRadius: 31,
+        backgroundColor: 'black'
+    },
+    postText: {
+        fontSize: 14,
+        maxWidth: 280,
+        marginTop: 20,
+        marginLeft: 17,
+        color: 'black'
+    },
+    postPhoto: {
+        backgroundColor: 'blue',
+        top: 61,
+        width: 314,
+        height: 140,
+        borderTopLeftRadius: 22,
+        borderTopRightRadius: 22,
+        borderBottomRightRadius: 29,
+        borderBottomLeftRadius: 29
+    }
+
+
+});
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,7 @@
+import { FontAwesome } from '@expo/vector-icons';
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
+import { Card } from '../components/Card';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -8,9 +10,14 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Image style={styles.logoImage} resizeMode={'contain'} source={require('../assets/images/communitycate.png')} />
+      <View style={styles.searchBar}>
+        <Text style={styles.searchText}>wyszukaj</Text>
+        <FontAwesome style={styles.searchIcon} size={24} name='search'/>
+      </View>
+      <Card>
+        
+      </Card>
     </View>
   );
 }
@@ -19,7 +26,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
   title: {
     fontSize: 20,
@@ -30,4 +37,29 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  logoImage: {
+    width: 340,
+    marginTop: 30
+  },
+  searchBar: {
+    maxWidth: 314,
+    width: 314,
+    height: 37,
+    maxHeight: 37,
+    borderRadius: 16,
+    borderColor: '#7A7878',
+    borderWidth: 1,
+    position: 'relative'
+  },
+  searchIcon: {
+    position: 'absolute',
+    right: 10,
+    top: 3
+  },
+  searchText: {
+    position: 'absolute',
+    top: 8,
+    left: 10,
+    color: '#7A7878'
+  }
 });
